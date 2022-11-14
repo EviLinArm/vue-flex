589fdda5d9243b409fb7f7e5aaacbef6e8f8033d
EviLinArm <zakharenko07@gmail.com>
2022-11-14 21:56:09 +1000

    Second commit

--- server/src/modules/stream/stream.controller.ts
+++ server/src/modules/stream/stream.controller.ts
@@ -1,12 +1,51 @@
 import { Router } from "express";
+import WebTorrent from "webtorrent";
 
 const router = Router();
+const client = new WebTorrent()
 
-router.get('/', (req, res) => {
-    console.log(123)
-    res.status(200).send({
-        hello: 'world'
+let state = {
+    progress: 0,
+    downloadSpeed: 0,
+    ratio: 0
+}
+
+let error
+
+client.on('error', (e: Error) => {
+    console.error('Error', e.message)
+    error = e.message
+})
+
+client.on('torrent', () => {
+    console.log(client.progress)
+    state = {
+        progress: Math.round(client.progress * 100 * 100) / 100,
+        downloadSpeed: client.downloadSpeed,
+        ratio: client.ratio
+    }
+})
+
+router.get('/add/:magnet', (req, res) => {
+    const magnet = req.params.magnet
+
+    client.add(magnet, (torrent) => {
+        const files = torrent.files.map(data => ({
+            name: data.name,
+            length: data.length
+        }))
+
+        res.status(200).send(files)
     })
+})
+
+router.get('/stats', (req, res) => {
+    state = {
+        progress: Math.round(client.progress * 100 * 100) / 100,
+        downloadSpeed: client.downloadSpeed,
+        ratio: client.ratio
+    }
+    res.status(200).send(state)
 })
 
 export default router
